0558701d133ac2a854523b9927a2a111abd9d09d
Post Games <snipz722@gmail.com>
2017-08-09 23:29:48 -0400

    WORKS

--- index.php
+++ index.php
@@ -13,17 +13,53 @@ $conn = mysqli_connect("localhost", "root", "root", "dddb");
 $sql = "SELECT * FROM post";
 
 $result = mysqli_query($conn, $sql);
-
+$curr = 0;
+$currM = 0;
 if (mysqli_num_rows($result) > 0) {
     while($row = mysqli_fetch_assoc($result)) {
+		$curr += 1;
+		$currM -= -5;
     echo "<div class='posts'><br>" . "<div class='posts-text'>" . $row["text"] . "</div>" ."</div>";
-	//echo "<button id='".$row['text']."'> ". $row["text"] . "</button>";
-	//echo "<button id='test'>test</button>";
+	echo "<button id='".$curr."'> ". $row["text"] . "</button>";
+	//echo "<button id='myBtn'>test</button>";
+	
+	echo '
+	<div id="'.$currM.'" class="modal">
+  <div class="modal-content">
+    <div class="modal-body">
+	<p>'.$row["text"].'</p>
+    </div>
+  </div>
+
+</div>
+
+<script>
+// Get the modal
+var modal = document.getElementById("'.$currM.'");
+
+// Get the button that opens the modal
+var btn = document.getElementById('.$curr.');
+
+// When the user clicks the button, open the modal 
+btn.onclick = function() {
+    modal.style.display = "block";
+}
+
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+}
+</script>';
 	
   }
 }
 
 ?>
+
+
 
 <div class="topdaily">
 
--- loginfb.php
+++ loginfb.php
@@ -7,15 +7,20 @@ $name = $_GET['name'];
 $ip = $_SERVER['REMOTE_ADDR'];
 $time = time();
 
-$sql = "SELECT fbid FROM loginfb WHERE fbid=$fbid";
-
+if(!isset($fbid) || !isset($name)) {
+	echo "a var is not set";
+}
+//$sql = "SELECT fbid FROM loginfb WHERE fbid=$fbid";
+$sql = "SELECT fbid FROM loginfb WHERE fbid='".$fbid."'";
 $result = mysqli_query($conn,$sql);
 
-
 if (mysqli_num_rows($result) == 0) {
-	$sqlinsert = "INSERT INTO loginfb (fbid,ip,regtime,name) VALUES ('$fbid','$ip',$time,'$name')";
+	//$sqlinsert = "INSERT INTO loginfb (fbid,ip,regtime,name) VALUES ('".$fbid."','".$ip."',".$time.",'".$name."'";
+	$sqlinsert = "INSERT INTO loginfb (fbid, ip, regtime, name) VALUES ('".$fbid."', '".$ip."', ".$time.", '".$name."')";
+	//insert into loginfb (fbid, ip, regtime,name) VALUES ("asd", "ip", 0, "NAME")
+	mysqli_query($conn,$sqlinsert);
+	echo "<script>window.location.replace('index.php');</script>";
+}else{
+echo "<script>window.location.replace('index.php');</script>";	
 }
-
-mysqli_query($conn,$sqlinsert);
-
 ?>
